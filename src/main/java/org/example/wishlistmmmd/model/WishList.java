5dbacba55c32273a3f8879fc87a52c2b61cb99e8
MetteMunch <meha0005@stud.kea.dk>
2024-10-31 08:44:32 +0100

    Initial branch commit. Tilføjet attributter, getter og setter i model klasser plus dependencies

--- src/main/java/org/example/wishlistmmmd/model/WishList.java
+++ src/main/java/org/example/wishlistmmmd/model/WishList.java
@@ -1,4 +1,49 @@
 package org.example.wishlistmmmd.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class WishList {
+
+    private int wishListID;
+    private String listName;
+    private LocalDate expireDate;
+    private List<Wish> wishesOnTheList;
+
+    public WishList(String listName, LocalDate expireDate) {
+        this.listName = listName;
+        this.expireDate = expireDate;
+    }
+
+    public int getWishListID() {
+        return wishListID;
+    }
+
+    public void setWishListID(int wishListID) {
+        this.wishListID = wishListID;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public List<Wish> getWishesOnTheList() {
+        return wishesOnTheList;
+    }
+
+    public void setWishesOnTheList(List<Wish> wishesOnTheList) {
+        this.wishesOnTheList = wishesOnTheList;
+    }
 }
