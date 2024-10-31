420408753bd4169778b687166a8f4eead8845b5d
MetteMunch <meha0005@stud.kea.dk>
2024-10-31 10:38:21 +0100

    Har lavet en tjek metode i Repository for at sikre der er hul igennem til databasen via singleton

--- src/main/java/org/example/wishlistmmmd/controller/WishController.java
+++ src/main/java/org/example/wishlistmmmd/controller/WishController.java
@@ -7,39 +7,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class WishController {
 
-    @GetMapping("/welcomePage")
-    public String welcomePage() {
-
-    }
-
-    @PostMapping("/login")
-    public String loginValidation() {
-
-    }
-
-    @GetMapping("/userProfileHomePage")
-    public String userProfileHomePage() {
-
-    }
-
-    @GetMapping("/createWishlist")
-    public String createWishList() {
-
-    }
-
-    @PostMapping("/saveWishlist")
-    public String saveWishlist() {
-        //return redirectUserProfile;
-    }
-
-    @GetMapping("/addWish")
-    public String addWish() {
-
-    }
-
-    @PostMapping("/saveWish")
-    public String saveWish() {
-        //return redirectSaveWish
-    }
+//    @GetMapping("/welcomePage")
+//    public String welcomePage() {
+//
+//    }
+//
+//    @PostMapping("/login")
+//    public String loginValidation() {
+//
+//    }
+//
+//    @GetMapping("/userProfileHomePage")
+//    public String userProfileHomePage() {
+//
+//    }
+//
+//    @GetMapping("/createWishlist")
+//    public String createWishList() {
+//
+//    }
+//
+//    @PostMapping("/saveWishlist")
+//    public String saveWishlist() {
+//        //return redirectUserProfile;
+//    }
+//
+//    @GetMapping("/addWish")
+//    public String addWish() {
+//
+//    }
+//
+//    @PostMapping("/saveWish")
+//    public String saveWish() {
+//        //return redirectSaveWish
+//    }
 
 }
